22f0f92fd15b757ef5e65c20e1618a78dbfa4edc
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-06-25 23:23:27 +0430

    add variable implementation in feature file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hello-cucumber
 a BDD tutorial
+full tutorial available on: https://cucumber.io/docs/guides/10-minute-tutorial/
--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -29,6 +29,11 @@ public class Stepdefs {
 		today = "Friday";
 	}
 
+	@Given("today is {string}")
+	public void today_is(String string) {
+		today = string;
+	}
+
 	@When("I ask whether it's Friday yet")
 	public void i_ask_whether_it_s_Friday_yet() {
 		actualAnswer = IsItFriday.isItFriday(today);
